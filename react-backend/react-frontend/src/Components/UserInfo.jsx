fe2a6464969422f95ae56286e26144858edfef54
Unknown <eileenvalera@gmail.com>
2017-09-19 11:57:36 +0100

    Adding testing

--- react-backend/react-frontend/src/Components/UserInfo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserInfo = ({ match }) => (
-  <div>
-    <h3>Hey {match.params.userId} {this.props} {JSON.stringify(match)}</h3>
-  </div>
-)
-
-export default UserInfo;
